fcb9673077c0d7566215d8eda62ca4d18db3e20d
root <root@c55ef46ffb76>
2019-05-03 19:40:41 +0000

    error handling; modularisation

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//"crypto/rand"
+	//"errors"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
@@ -74,55 +75,92 @@ func (h *handler) SetCACert(filename string) {
 	fmt.Printf("Loaded CA cert: %s\n", filename)
 }
 
+type JSONrequest struct {
+	method		string
+	urlpath		string
+	jsonInterface	interface{}
+	decodeErr	interface{}
+}
+
+func (j *JSONrequest) DecodeReq(req *http.Request) {
+	j.method = req.Method
+	j.urlpath = req.URL.Path
+	decoder := json.NewDecoder(req.Body)
+	j.decodeErr = decoder.Decode(&j.jsonInterface)
+	defer req.Body.Close()
+}
+
 func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
-	method := req.Method
-	urlpath := req.URL.Path
-	decoder := json.NewDecoder(req.Body)
-	var t interface{}
-	decoder.Decode(&t)
-	defer req.Body.Close()
-	fmt.Println(method)
-	fmt.Println(urlpath)
+	var jsonReq JSONrequest
+	var respBody string
+	jsonReq.DecodeReq(req)
+	if jsonReq.decodeErr != nil {
+		respBody = "{\"error\": \"json decode error\"}"
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(respBody))
+		return
+	}
+	//method := req.Method
+	//urlpath := req.URL.Path
+	//decoder := json.NewDecoder(req.Body)
+	//var t interface{}
+	//if err := decoder.Decode(&t); err != nil {
+	//	fmt.Println(err)
+	//}
+	//defer req.Body.Close()
+	//fmt.Println(method)
+	//fmt.Println(urlpath)
 	peerCerts := req.TLS.PeerCertificates
-	dbg2, err := json.Marshal(t)
-	fmt.Println(err)
-	fmt.Println(string(dbg2))
-	fmt.Println(peerCerts[0].DNSNames)
+	//dbg2, err := json.Marshal(t)
+	//fmt.Println(err)
+	//fmt.Println(string(dbg2))
+	//fmt.Println(peerCerts[0].DNSNames)
 
 	peerDNSNames := peerCerts[0].DNSNames
-	fmt.Println(peerDNSNames)
+	//fmt.Println(peerDNSNames)
 
-	urlPathSplit := strings.Split(urlpath, "/")
-	fmt.Println(urlPathSplit)
-	var DNSName string
-	if "/api/v1/servers" == strings.Join(urlPathSplit[:4], "/") {
-		if "zones" == urlPathSplit[5] {
-			if 7 == len(urlPathSplit) {
-				DNSName = urlPathSplit[6]
-			} else {
-				DNSName = t.(map[string]interface{})["name"].(string)
-			}
-		}
-		fmt.Println("Hallo")
-	}
+	urlPathSplit := strings.Split(jsonReq.urlpath, "/")
+	DNSName := getDNSName(jsonReq.urlpath, jsonReq.jsonInterface)
 
 	fmt.Println(DNSName)
-	var respBody string
 	if ok, name := IsSubset([]string{DNSName}, peerDNSNames); ok {
-		body, _ := json.Marshal(t)
+		body, _ := json.Marshal(jsonReq.jsonInterface)
 		bodyrdr := strings.NewReader(string(body))
-		r, _ := http.NewRequest(method, "http://localhost:8081"+urlpath, bodyrdr)
+		r, _ := http.NewRequest(jsonReq.method, "http://localhost:8081"+jsonReq.urlpath, bodyrdr)
 		r.Header.Set("X-Api-Key", "changeme")
 		resp, _ := http.DefaultClient.Do(r)
+
+		w.WriteHeader(resp.StatusCode)
+
 		respBA, _ := ioutil.ReadAll(resp.Body)
 		respBody = string(respBA)
 		defer resp.Body.Close()
 	} else {
 		fmt.Printf("DNS name not authorized by client certificate: %s\n", name)
+		w.WriteHeader(http.StatusForbidden)
+		respBody = fmt.Sprintf("{\"error\": \"name '%s' not authorized by client certificate\"}", DNSName)
 	}
 	fmt.Println(strings.Join(urlPathSplit[:4], "/"))
 	w.Write([]byte(respBody))
+}
+
+
+func getDNSName(urlpath string, jsonInterface interface{}) (string) {
+	urlPathSplit := strings.Split(urlpath, "/")
+	DNSName := ""
+	if "/api/v1/servers" == strings.Join(urlPathSplit[:4], "/") {
+		if "zones" == urlPathSplit[5] {
+			fmt.Printf("len: %d\n", len(urlPathSplit))
+			if 7 == len(urlPathSplit) {
+				DNSName = urlPathSplit[6]
+			}
+			if "" == DNSName {
+				DNSName = jsonInterface.(map[string]interface{})["name"].(string)
+			}
+		}
+	}
+	return DNSName
 }
 
 func IsSubset(x, y []string) (bool, string) {
